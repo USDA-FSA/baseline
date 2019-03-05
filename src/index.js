882108d979673062dd604c722befb3c0c0c1ec1f
Casey Hill <bignamehere@gmail.com>
2019-03-05 09:22:28 -0600

    added updates associated with JS alias issue and SASS variable issues [init #6]

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@
 //
 import jQuery from 'jquery';
 
-// FSA-Style imports from node_modules - variables set in webpack.common.js
-import 'FSA-STYLE-SCSS';
-import 'FSA-STYLE-JS';
-
 
 // Project-specific styles
 
