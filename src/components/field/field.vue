b5343b48100ec61c7f56233cef5b81ea6137f689
Casey Hill <bignamehere@gmail.com>
2019-08-15 16:18:22 -0500

    created working version of field component

--- src/components/field/field.vue
+++ src/components/field/field.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="fsa-field v-if='ERROR' fsa-field--error {{ CLASS_EXTRA }}">
-    <label class="fsa-field__label" for="{{ ID }}" id="{{ ID }}_label">
-      {{ LABEL }}
-      <span v-if="LABEL_DESC" class="fsa-field__label-desc">{{ LABEL_DESC }}</span>
+  <div :class="'fsa-field '+ERROR_FIELD+' '+ CLASS_EXTRA">
+    <label
+      class="fsa-field__label"
+      :for="ID"
+      :id="ID +'_label'">
+        {{ LABEL }} <slot v-slot:labelDescribe></slot>
     </label>
     <input
-      class="fsa-input fsa-field__item {{ CLASS }}"
-      {{#if BEHAVIOR}}data-behavior="{{ BEHAVIOR }}"{{/if}}
-      value="{{ INPUT_VALUE }}"
-      id="{{ ID }}"
-      name="{{ ID }}"
-      type="text"
-      {{#if MESSAGE}}aria-describedby="{{ ID }}__error-message"{{/if}}
-      {{#if REQUIRED}}required="required" aria-required="true"{{/if}}
-      aria-describedby="{{ HELP_ID }}"
-    >
-    
-    <span v-if="HELP" id="{{ HELP_ID }}" class="fsa-field__help">{{ HELP }}</span>    
-    <span v-if="MESSAGE" class="fsa-field__message" id="{{ ID }}__error-message" role="alert">{{ MESSAGE }}</span>
-
+      :class="'fsa-input fsa-field__item'"
+      :id="ID"
+      :aria-required="ARIA_REQUIRED"
+      :required="REQUIRED"
+      :name="ID"
+      :type="INPUT_TYPE"
+      :value="INPUT_VALUE"
+      :data-behavior="BEHAVIOR"
+      :aria-describedby="ARIA_DESCRIBEDBY"
+      >
+    <slot v-slot:help></slot>
+    <slot v-slot:message></slot>
   </div>
 </template>
 <script>
@@ -26,14 +26,24 @@ export default {
   props: {
     ID: String,
     LABEL: String,
-    LABEL_DESC: String,
-    CLASS: String,
-    BEHAVIOR: String,
+    REQUIRED: String,
     INPUT_VALUE: String,
+    INPUT_TYPE: String,
+    BEHAVIOR: String,
+    CLASS_EXTRA: String,
+    ERROR_FIELD: String,
     MESSAGE: String,
-    HELP_ID: String,
     HELP: String,
-    EXTRA_CLASS: String,
+    ARIA_REQUIRED: String,
+    ARIA_DESCRIBEDBY: String
+    
+  },
+
+  components: {
+    
+  },
+  computed: {
+     
   }
 }
 </script>
---
+++ src/components/field/fieldX.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="fsa-field {{ CLASS_EXTRA }}" v-bind:class="{ 'fsa-field--error': ERROR }">
+    <label class="fsa-field__label" for="{{ ID }}" id="{{ ID }}_label">
+      {{ LABEL }}
+      <span v-if="LABEL_DESC" class="fsa-field__label-desc">{{ LABEL_DESC }}</span>
+    </label>
+    <input
+      class="fsa-input fsa-field__item {{ CLASS }}"
+      value="{{ INPUT_VALUE }}"
+      id="{{ ID }}"
+      name="{{ ID }}"
+      type="text"
+      aria-describedby="{{ ID }}__error-message"
+      required="{{ REQUIRED }}"
+      v-bind:aria-required="{ 'true': REQUIRED }"
+      aria-describedby="{{ HELP_ID }}"
+    >
+    
+    <span v-if="HELP" id="{{ HELP_ID }}" class="fsa-field__help">{{ HELP }}</span>    
+    <span v-if="MESSAGE" class="fsa-field__message" id="{{ ID }}__error-message" role="alert">
+      {{ MESSAGE }}
+    </span>
+
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    ID: String,
+    LABEL: String,
+    LABEL_DESC: String,
+    CLASS: String,
+    ERROR: Boolean,
+    INPUT_VALUE: String,
+    MESSAGE: String,
+    HELP_ID: String,
+    HELP: String,
+    EXTRA_CLASS: String,
+  }
+}
+</script>
